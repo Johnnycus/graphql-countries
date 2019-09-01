efb19b849fffd523e073492d63d8141c048a0dc1
Johnnycus <sirjsx@gmail.com>
2019-09-02 01:21:44 +0600

    Remove unnecessary react-sticky

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,13 +1,10 @@
 import React, { useEffect } from 'react'
 import { useQuery, useApolloClient } from '@apollo/react-hooks'
 import styled from 'styled-components'
-import { StickyContainer, Sticky } from 'react-sticky'
 
 import { COUNTRY_QUERY } from '../utils/queries'
 
-const Container = styled.div`
-  /* width: auto !important; */
-`
+const Container = styled.div``
 
 const Country = ({
   match: {
@@ -30,19 +27,12 @@ const Country = ({
   if (error) return <p>Error!</p>
 
   return (
-    // <StickyContainer>
-    //   <Sticky>
-    //     {({ style }) => (
-    // <Container style={style}>
     <Container>
       <h1>{country.name}</h1>
 
       <h2>Currency: {country.currency}</h2>
       <h2>Area Code: +{country.phone}</h2>
     </Container>
-    //     )}
-    //   </Sticky>
-    // </StickyContainer>
   )
 }
 
